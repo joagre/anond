135c22e5034bd9d687d94fc6bf845f5d32e6a7ea
joagre <joagre@gmail.com>
2013-11-13 12:22:45 -0800

    fixed more dialyzer errors

--- lib/util/include/log_serv.hrl
+++ lib/util/include/log_serv.hrl
@@ -4,20 +4,20 @@
 -record(daemon_log_info, {
           enabled :: boolean(),
           tty     :: boolean(),
-          file    :: {Enabled :: boolean(), file:filename()}
+          file    :: {Enabled :: boolean(), Filename :: binary()}
         }).
 
 -record(dbg_log_info, {
           enabled       :: boolean(),
           tty           :: boolean(),
-          file          :: {Enabled :: boolean(), file:filename()},
+          file          :: {Enabled :: boolean(), Filename :: binary()},
           module_filter :: [{'show', binary()} | {'hide', binary()}]
          }).
 
 -record(error_log_info, {
           enabled :: boolean(),
           tty     :: boolean(),
-          file    :: {Enabled :: boolean(), file:filename()}
+          file    :: {Enabled :: boolean(), Filename :: binary()}
         }).
 
 -endif.
