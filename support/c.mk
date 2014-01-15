8b0fbc49b1a63ed40502ef736b10a2a207611662
joagre <joagre@gmail.com>
2014-01-15 08:26:43 -0800

    salt is not building properly

--- support/c.mk
+++ support/c.mk
@@ -1,3 +1,2 @@
 OS=$(shell uname -s)
-CFLAGS=-I$(ERL_TOP)/lib/erlang/usr/include -I/usr/include/sodium -I/usr/local/include/sodium -I/opt/local/include/sodium
-LIBSODIUM=/opt/local/lib/libsodium.a
+CFLAGS=-I$(ERL_TOP)/lib/erlang/usr/include -I/usr/local/include/sodium
