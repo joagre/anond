708e898258ebdd75907d65e08377c4a5568f4912
joagre <joagre@gmail.com>
2014-07-03 11:44:22 +0200

    preparing for lab setup

--- lib/test/lab/vbox/node/install_anond.sh
+++ lib/test/lab/vbox/node/install_anond.sh
@@ -7,13 +7,24 @@ if [ $# -ne 1 ]; then
 fi
 
 DISTRO=${1}
+NA=1.1.1.1:5000
+DS=2.2.2.2:6700
+MODE=node
 
-killall beam.smp
-killall tail
-killall screen
-rm -fr install
-mkdir install
-mv ${DISTRO}.tgz install
-cd install
+killall -q beam.smp
+killall -q tail
+killall -q screen
+rm -fr local
+mkdir local
+cp ${DISTRO}.tgz local
+cd local
 tar xf ${DISTRO}.tgz
 cd ${DISTRO}
+
+case "${MODE}" in
+    node)
+        sed -e "s/0.0.0.0:50000/${NA}/g" -e "s/ds.anond.org:6700/${DS}/g" etc/anond.conf > etc/local-anond.conf
+        ;;
+    ds)
+        sed -e "s/0.0.0.0:6700/${DS}/g" etc/anond.conf > etc/local-anond.conf
+esac
